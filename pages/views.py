7087569411296e4e6a81f93899b9080d14426cec
pradeep prasad <pprasad@collegeboard.org>
2018-04-05 10:23:40 -0400

    updating the comment

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
+'''home page view'''
 def homePageView(request):
     return HttpResponse("Hello World!")
 # Create your views here.
